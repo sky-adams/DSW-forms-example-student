2ce5d4bb21299fdc022c642f16a8ce31879c7da7
sky-adams <skyadams@sbunified.org>
2017-10-08 18:53:45 -0700

    Create webapp.py

---
+++ webapp.py
@@ -0,0 +1,14 @@
+from flask import Flask, url_for, render_template, request
+
+app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
+
+@app.route("/")
+def render_main():
+    return render_template('home.html')
+
+@app.route("/response")
+def render_response():
+    
+    
+if __name__=="__main__":
+    app.run(debug=False, port=54321)

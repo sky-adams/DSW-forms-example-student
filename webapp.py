3b8f3771f5a9478f91353fb2afa128cdc01bca37
sky-adams <skyadams@sbunified.org>
2019-10-10 09:12:38 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -8,6 +8,11 @@ def render_main():
 
 @app.route("/response")
 def render_response():
+    favorite_color = request.args['color'] #get user's input for color input
+    if favorite_color == "pink":
+        response = "That's my favorite color, too!"
+    else:
+        response = "My favorite color is pink."
     return render_template('response.html')
     
     
